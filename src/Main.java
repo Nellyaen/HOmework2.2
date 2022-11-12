41ad892311c15819093ad685c91cc012be1b1a0b
nellya <nellya.ziganshina@gmail.com>
2022-11-12 22:56:33 +0100

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //задание 1
+       var friend = 19;
+       System.out.println(friend);
+       friend = friend + 2;
+       System.out.println(friend);
+       friend = friend / 7;
+       System.out.println(friend);
+
+       //задание 2
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+
+
     }
 }
